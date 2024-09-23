4c1a8923a98d1b2e8ad9939eaea9df5ced848152
Kyazaray <nunescoelhorenan@proton.me>
2024-09-23 02:06:13 -0300

    style (removed dark classes)

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -55,29 +55,29 @@
         @yield('body')
         @livewireScripts
 
-        <footer class="bg-white dark:bg-gray-900">
+        <footer class="bg-white ">
             <div class="container px-6 py-8 mx-auto">
                 <div class="flex flex-col items-center text-center">
                     <a href="#">
                         <img class="w-auto h-7 rounded" src="{{ asset('img/logo-wb.png') }}" alt="">
                     </a>
         
-                    <p class="font-family max-w-full mx-auto mt-4 text-gray-500 dark:text-gray-400">South Store, a melhor loja de dispositivos Apple de Canguçu e região!</p>
+                    <p class="font-family max-w-full mx-auto mt-4 text-gray-500">South Store, a melhor loja de dispositivos Apple de Canguçu e região!</p>
         
                     
                 </div>
         
-                <hr class="my-10 border-gray-200 dark:border-gray-700" />
+                <hr class="my-10 border-gray-200" />
         
                 <div class="font-family flex flex-col items-center sm:flex-row sm:justify-between">
                     <p class="text-sm text-gray-500">© Copyright 2022 - {{ date('Y') }}. Alguns direitos reservados.</p>
         
                     <div class="flex mt-3 -mx-2 sm:mt-0">
-                        <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Teams </a>
+                        <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500" aria-label="Reddit"> Teams </a>
         
-                        <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Privacy </a>
+                        <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500" aria-label="Reddit"> Privacy </a>
         
-                        <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500 dark:hover:text-gray-300" aria-label="Reddit"> Cookies </a>
+                        <a href="#" class="mx-2 text-sm text-gray-500 transition-colors duration-300 hover:text-gray-500" aria-label="Reddit"> Cookies </a>
                     </div>
                 </div>
             </div>
--- resources/views/livewire/landing.blade.php
+++ resources/views/livewire/landing.blade.php
@@ -100,24 +100,24 @@
   updateHero(); // Initial call
 </script>
 
-    <section class="bg-gray-50 py-8 antialiased dark:bg-gray-900 md:py-12">
+    <section class="bg-gray-50 py-8 antialiased md:py-12">
         <div class="mx-auto max-w-screen-xl px-4 2xl:px-0">
 
-        <h2 class="font-family mt-3 mb-3 text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">Adicionados Recentemente</h2>
+        <h2 class="font-family mt-3 mb-3 text-xl font-semibold text-gray-900 sm:text-2xl">Adicionados Recentemente</h2>
           <div class="mb-4 grid gap-4 sm:grid-cols-2 md:mb-8 lg:grid-cols-3 xl:grid-cols-4">
 
-            <div class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
+            <div class="rounded-lg border border-gray-200 bg-white p-6 shadow-sm">
               <div class="h-56 w-full">
                 <a href="#">
-                  <img class="mx-auto h-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/iphone-light.svg" alt="" />
-                  <img class="mx-auto hidden h-full dark:block" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/iphone-dark.svg" alt="" />
+                  <img class="mx-auto h-full " src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/iphone-light.svg" alt="" />
+                  <img class="mx-auto hidden h-full" src="https://flowbite.s3.amazonaws.com/blocks/e-commerce/iphone-dark.svg" alt="" />
                 </a>
               </div>
       
               <div class="pt-6">
         
       
-                <a href="#" class="font-family text-lg font-semibold leading-tight text-gray-900 hover:underline dark:text-white">Apple iPhone 15 Pro Max</a>
+                <a href="#" class="font-family text-lg font-semibold leading-tight text-gray-900 hover:underline">Apple iPhone 15 Pro Max</a>
       
                 <div class="mt-2 flex items-center gap-2">
                   <div class="flex items-center">
@@ -142,13 +142,13 @@
                     </svg>
                   </div>
       
-                  <p class="text-sm font-medium text-gray-900 dark:text-white">4.9</p>
-                  <p class="text-sm font-medium text-gray-500 dark:text-gray-400">(1,233)</p>
+                  <p class="text-sm font-medium text-gray-900">4.9</p>
+                  <p class="text-sm font-medium text-gray-500">(1,233)</p>
                 </div>
       
                 <ul class="mt-2 flex items-center gap-4">
                   <li class="flex items-center gap-2">
-                    <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <svg class="h-4 w-4 text-gray-500" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path
                         stroke="currentColor"
                         stroke-linecap="round"
@@ -157,21 +157,21 @@
                         d="m7.171 12.906-2.153 6.411 2.672-.89 1.568 2.34 1.825-5.183m5.73-2.678 2.154 6.411-2.673-.89-1.568 2.34-1.825-5.183M9.165 4.3c.58.068 1.153-.17 1.515-.628a1.681 1.681 0 0 1 2.64 0 1.68 1.68 0 0 0 1.515.628 1.681 1.681 0 0 1 1.866 1.866c-.068.58.17 1.154.628 1.516a1.681 1.681 0 0 1 0 2.639 1.682 1.682 0 0 0-.628 1.515 1.681 1.681 0 0 1-1.866 1.866 1.681 1.681 0 0 0-1.516.628 1.681 1.681 0 0 1-2.639 0 1.681 1.681 0 0 0-1.515-.628 1.681 1.681 0 0 1-1.867-1.866 1.681 1.681 0 0 0-.627-1.515 1.681 1.681 0 0 1 0-2.64c.458-.361.696-.935.627-1.515A1.681 1.681 0 0 1 9.165 4.3ZM14 9a2 2 0 1 1-4 0 2 2 0 0 1 4 0Z"
                       />
                     </svg>
-                    <p class="text-sm font-medium text-gray-500 dark:text-gray-400">Best Seller</p>
+                    <p class="text-sm font-medium text-gray-500">Best Seller</p>
                   </li>
       
                   <li class="flex items-center gap-2">
-                    <svg class="h-4 w-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                    <svg class="h-4 w-4 text-gray-500 " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="M8 7V6c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1h-1M3 18v-7c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
                     </svg>
-                    <p class="text-sm font-medium text-gray-500 dark:text-gray-400">Best Price</p>
+                    <p class="text-sm font-medium text-gray-500">Best Price</p>
                   </li>
                 </ul>
       
                 <div class="mt-4 flex items-center justify-between gap-4">
-                  <p class="font-family text-2xl font-extrabold leading-tight text-gray-900 dark:text-white">R$5,500</p>
+                  <p class="font-family text-2xl font-extrabold leading-tight text-gray-900">R$5,500</p>
       
-                  <button type="button" class="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4  focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+                  <button type="button" class="inline-flex items-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4  focus:ring-primary-300">
                     <svg class="-ms-2 me-2 h-5 w-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
                       <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-3-3h6" />
                     </svg>
