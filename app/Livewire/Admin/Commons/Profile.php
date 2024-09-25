803e3792f24d43e17a5a5cec7f8cebfd8d9e635a
Kyazaray <nunescoelhorenan@proton.me>
2024-09-24 23:40:17 -0300

    feat (pqp)

--- app/Livewire/Admin/Commons/Profile.php
+++ app/Livewire/Admin/Commons/Profile.php
@@ -4,16 +4,29 @@ namespace App\Livewire\Admin\Commons;
 
 use Livewire\Component;
 use App\Models\User;
+use Sonata\GoogleAuthenticator\GoogleAuthenticator;
 
 class Profile extends Component
 {
+    use Notifiable;
     public string $name;
     public string $email;
+    public string $secret;
+    public string $qrCodeURL;
+    public string $verifycode;
 
     public function mount()
     {
         $this->name = auth()->user()->name;
         $this->email = auth()->user()->email;
+
+        $googleAuthenticator = new GoogleAuthenticator();
+        $this->secret = $googleAuthenticator->generateSecret();
+        $this->qrCodeURL = $googleAuthenticator->getURL('SouthStore', $this->email, $this->secret);
+    }
+
+    public function testFeatures()
+    {
     }
 
     public function userEdit()
@@ -26,8 +39,22 @@ class Profile extends Component
         redirect()->route('profile');
     }
 
+    public function verifyCode()
+    {
+        $googleAuthenticator = new GoogleAuthenticator();
+        $isValid = $googleAuthenticator->checkCode($this->secret, $this->twofacode);
+        if ($isValid) {
+            $this->isOpen = false;
+            $this->user->twofa_secret = $this->secret;
+            $this->user->save();
+    
+        }
+    }
+
     public function render()
     {
-        return view('livewire.admin.commons.profile')->layout('components.layouts.admin');
+        return view('livewire.admin.commons.profile', [
+            'qrCodeURL' => $this->qrCodeURL,
+        ])->layout('components.layouts.admin');
     }
 }
--- resources/views/livewire/admin/commons/profile.blade.php
+++ resources/views/livewire/admin/commons/profile.blade.php
@@ -3,11 +3,9 @@
 
     @if (auth()->user()->two_factor_secret == NULL)
         <div id="alert-2" class="flex items-center p-4 mb-4 text-red-800 rounded-lg bg-red-50" role="alert">
-            <svg class="flex-shrink-0 w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
-            </svg>
+            <i class="fa-solid fa-triangle-exclamation"></i>
             <span class="sr-only">Info</span>
-            <div class="ms-3 text-sm font-medium">
+            <div class="ms-2 text-sm font-medium">
               <b>AVISO:</b> A autenticação de dois fatores não está ativa, por favor, habilite-a para uma maior segurança.
             </div>
         </div>
@@ -21,9 +19,6 @@
         </div>
         <div class="sm:w-2/3 p-4">
             <div class="border-b border-gray-900/10 pb-3">
-                
-            
-
                     <div class="mt-3 grid grid-cols-1 gap-x-6 gap-y-4 sm:grid-cols-6">
                         <div class="sm:col-span-2 sm:col-start-1">
                             <label for="city" class="block text-sm font-medium leading-6 text-gray-900">Nome</label>
@@ -42,16 +37,94 @@
                         <div class="sm:col-span-2">
                             <label for="region" class="block text-sm font-medium leading-6 text-gray-900">2FA</label>
                             <div class="mt-2">
-                                <button wire:click="userEdit" class="text-sm bg-blue-600 hover:bg-blue-700 text-white py-2 px-3 rounded-md">Autenticação de dois fatores</button>
+                                <!-- if user have 2fa -->
+                                @if (auth()->user()->two_factor_secret == NULL)
+                                    <button onclick="openModal()" class="text-sm bg-blue-600 hover:bg-blue-700 text-white py-2 px-3 rounded-md">Autenticação de dois fatores</button>
+                                @endif
                             </div>
                         </div>
                     </div>
             </div>
             <div class="mt-3">
                 <button wire:click="userEdit" class="text-sm bg-blue-600 border border-blue-600 hover:bg-blue-700 text-white py-1 px-3 rounded-md">Save</button>
-                <button class="text-sm bg-transparent border border-blue-600 hover:bg-gray-200 py-1 px-3 rounded-md">Alterar Senha</button>
+                <button wire:click="testFeatures" class="text-sm bg-transparent border border-blue-600 hover:bg-gray-200 py-1 px-3 rounded-md">Alterar Senha</button>
 
             </div>
         </div>
     </div>
+    <div class="modal hidden fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center">
+        <div class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+            <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+              <div class="sm:flex sm:items-start">
+                <div class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <i class="fa fa-lock text-green-600"></i>
+                </div>
+                <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                  <h3 class="text-base font-semibold leading-6 text-gray-900" id="modal-title">Ativar autenticação em duas etapas</h3>
+                  <div class="mt-2">
+                    <div class="flex justify-center">
+                        <img src="{{ $qrCodeURL }}">
+                    </div>
+                    <p class="text-sm text-gray-500 mt-3">Basta escanear o Qr Code acima e informar o código abaixo.</p>
+                    <input wire:model="verifycode" type="number" name="verifycode" id="verifycode" min="0" class="block w-full px-3 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 sm:text-sm sm:leading-6">                
+                </div>
+                </div>
+              </div>
+            </div>
+            <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+              <button type="button" class="inline-flex w-full justify-center rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 sm:ml-3 sm:w-auto">Ativar</button>
+              <button onclick="closeModal()" type="button" class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto">Cancelar</button>
+            </div>
+          </div>
+    </div>
+    <style>
+        .modal {
+          display: none;
+          /* Additional styles for modal */
+        }
+
+        .modal.open {
+          display: flex;
+          animation: fadeIn 0.3s ease-in-out;
+        }
+
+        .modal.closing {
+          animation: fadeOut 0.3s ease-in-out;
+        }
+
+        @keyframes fadeIn {
+          from {
+            opacity: 0;
+          }
+          to {
+            opacity: 1;
+          }
+        }
+
+        @keyframes fadeOut {
+          from {
+            opacity: 1;
+          }
+          to {
+            opacity: 0;
+          }
+        }
+      </style>
+      <script>
+        function openModal() {
+          const modal = document.querySelector('.modal');
+          modal.classList.add('open');
+        }
+        
+        function closeModal() {
+          const modal = document.querySelector('.modal');
+          modal.classList.remove('open');
+          modal.classList.add('closing');
+        
+          setTimeout(() => {
+            modal.classList.remove('closing');
+            modal.classList.add('hidden'); // Add this line to hide the modal after the animation
+          }, 300); // Adjust the timeout duration as needed
+        }
+      </script>
 </div>
